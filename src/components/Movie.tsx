29c464b402e58c56acb4637d3098c1144d5cd404
frojak4 <Frojak4@gmail.com>
2024-09-18 14:40:06 +0200

    Update Movie.tsx

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -9,7 +9,7 @@ type MovieProps = {
 
 const Movie = ({name, year, director, score}: MovieProps) => {
   return (
-    <div className="bg-slate-800 mx-32 rounded-3xl h-16 my-3 flex items-center justify-between overflow-auto">
+    <div className="bg-slate-800 mx-32 rounded-3xl h-16 my-3 flex items-center justify-between">
         <span className="ml-8 p-1 text-4xl text-slate-400 flex-1 truncate">{name} <span className="text-slate-600">by {director}</span></span>
         <div className="flex justify-between w-96">
         <span className="text-4xl text-white text-center truncate">{year}</span>
